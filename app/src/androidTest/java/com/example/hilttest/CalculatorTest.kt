bb7e20670c3764e67642bcd995ee49e231d472a6
lyh990517 <lyh990517@naver.com>
2023-11-05 11:11:56 +0900

    [Test] Create a class to test the fake interface

---
+++ app/src/androidTest/java/com/example/hilttest/CalculatorTest.kt
@@ -0,0 +1,34 @@
+package com.example.hilttest
+
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import junit.framework.TestCase.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+
+@HiltAndroidTest
+class CalculatorTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var fakeCalculator: FakeCalculator
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun `숫자를_더한다`() {
+        val a = 1
+        val b = 2
+
+        val result = fakeCalculator.add(a, b)
+
+        assertEquals(3, result)
+    }
+}
