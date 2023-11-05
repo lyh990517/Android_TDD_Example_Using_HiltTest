46b0e20a092809b7c9bd9a8a100baa8f7cbf46c6
lyh990517 <lyh990517@naver.com>
2023-11-05 11:29:26 +0900

    [App] Edit Hilt Module

--- app/src/main/java/com/example/hilttest/di/CalculatorModule.kt
+++ app/src/main/java/com/example/hilttest/di/CalculatorModule.kt
@@ -1,5 +1,8 @@
 package com.example.hilttest.di
 
+import com.example.hilttest.Calculator
+import com.example.hilttest.CalculatorImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,4 +10,6 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class CalculatorModule {
+    @Binds
+    abstract fun bindsCalculator(calculatorImpl: CalculatorImpl) : Calculator
 }
